d191cd96aad5533252f77d2c15f3bafd191b31b1
sagar samvedi <sagarsamvedi31@gmail.com>
2023-12-04 18:15:09 +0530

    Created Tic Tac Toe Game Using React

---
+++ TicTacToe/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import Board from './Components/Board'
+
+function App() {
+   return (
+    <>
+    <Board/>
+    </>
+   ) 
+}
+
+export default App
---
+++ TicTacToe/src/Components/Square.jsx
@@ -0,0 +1,12 @@
+const Square = (props) => {
+  return (
+    <>
+      <div className="square" onClick={props.onClick}>
+        <h4>{props.value}</h4>
+      </div>
+
+    </>
+  );
+};
+
+export default Square;
