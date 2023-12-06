63c3f38f8c0199c3886308ea3b8767ae5524059c
sagar samvedi <sagarsamvedi31@gmail.com>
2023-12-06 14:34:03 +0530

    Added Winning Effect Animation

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -1,9 +1,23 @@
+import { useEffect } from "react";
 import { useState } from "react";
 import Square from "./Square";
 
 const Board = () => {
   const [state, setState] = useState(Array(9).fill(null));
   const [isXTurn, setIsXTurn] = useState(true);
+  const [WinningSquare, setWinningSquare] = useState([])
+
+  const winnerLogic = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
   const handleClick = (index) => {
     // checking if the square is already filled
     if (state[index] != null) {
@@ -18,16 +32,6 @@ const Board = () => {
   };
 
   const checkWinner = () => {
-    const winnerLogic = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
     for (let logic of winnerLogic) {
       const [a, b, c] = logic;
       if (state[a] !== null && state[a] === state[b] && state[a] === state[c]) {
@@ -44,14 +48,29 @@ const Board = () => {
     }
     return true; // All squares are filled, no winner detected, it's a draw
   };
-
-  const isDraw = checkDraw();
-
-  const isWinner = checkWinner();
-
   const handleReset = () => {
     setState(Array(9).fill(null));
   };
+ 
+
+  useEffect(() => {
+    const winner = checkWinner();
+    if (winner) {
+      const winningSquares = winnerLogic.find(logic => {
+      const [a,b,c] = logic;
+      return state[a] === winner && state[b] === winner && state[c] === winner
+      });
+
+      setWinningSquare(winningSquares || []);
+      console.log('winning square',winningSquares);
+    } else {
+      setWinningSquare([]);
+    }
+  }, [state]);
+
+  const isDraw = checkDraw();
+  const isWinner = checkWinner();
+
 
   return (
     <>
@@ -63,19 +82,19 @@ const Board = () => {
             </p>
           </div>
           <div className="rows">
-            <Square onClick={() => handleClick(0)} value={state[0]} />
-            <Square onClick={() => handleClick(1)} value={state[1]} />
-            <Square onClick={() => handleClick(2)} value={state[2]} />
+            <Square onClick={() => handleClick(0)} value={state[0]} isWinningSquare={WinningSquare.includes(0)}/>
+            <Square onClick={() => handleClick(1)} value={state[1]} isWinningSquare={WinningSquare.includes(1)}/>
+            <Square onClick={() => handleClick(2)} value={state[2]} isWinningSquare={WinningSquare.includes(2)}/>
           </div>
           <div className="rows">
-            <Square onClick={() => handleClick(3)} value={state[3]} />
-            <Square onClick={() => handleClick(4)} value={state[4]} />
-            <Square onClick={() => handleClick(5)} value={state[5]} />
+            <Square onClick={() => handleClick(3)} value={state[3]} isWinningSquare={WinningSquare.includes(3)}/>
+            <Square onClick={() => handleClick(4)} value={state[4]} isWinningSquare={WinningSquare.includes(4)}/>
+            <Square onClick={() => handleClick(5)} value={state[5]} isWinningSquare={WinningSquare.includes(5)}/>
           </div>
           <div className="rows">
-            <Square onClick={() => handleClick(6)} value={state[6]} />
-            <Square onClick={() => handleClick(7)} value={state[7]} />
-            <Square onClick={() => handleClick(8)} value={state[8]} />
+            <Square onClick={() => handleClick(6)} value={state[6]} isWinningSquare={WinningSquare.includes(6)}/>
+            <Square onClick={() => handleClick(7)} value={state[7]} isWinningSquare={WinningSquare.includes(7)}/>
+            <Square onClick={() => handleClick(8)} value={state[8]} isWinningSquare={WinningSquare.includes(8)}/>
           </div>
           <div className="result">
             {isWinner && (
--- src/Components/Square.jsx
+++ src/Components/Square.jsx
@@ -1,7 +1,10 @@
 const Square = (props) => {
+  const squareStyle = {
+    animation: props.isWinningSquare ? "blink 1s infinite" : "none",
+  };
   return (
     <>
-      <div className="square" onClick={props.onClick}>
+      <div className="square" onClick={props.onClick} style={squareStyle}>
         <h4>{props.value}</h4>
       </div>
     </>
